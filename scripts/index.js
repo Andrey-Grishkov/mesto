7d97c8906b004e230a8926842af0759a8ed50855
Andrey-Grishkov <andreygorm@gmail.com>
2022-04-03 15:44:26 +0300

    fix: change popup input class

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const profileEdit = document.querySelector('.profile__edit');
 const popupWindow = document.querySelector('.popup');
 const popupCloseBtn = popupWindow.querySelector('.popup__close');
 const popupSubmit = popupWindow.querySelector('.popup__form');
-const userNameInput = popupWindow.querySelector('.popup__name');
-const userAboutInput = popupWindow.querySelector('.popup__user-about');
+const userNameInput = popupWindow.querySelector('.popup__user-input_input_name');
+const userAboutInput = popupWindow.querySelector('.popup__user-input_input_user-about');
 const profileName = document.querySelector('.profile__name');
 const profileUserAbout = document.querySelector('.profile__user-about');
 
