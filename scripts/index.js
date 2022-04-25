077f0619f587c3f77ab2eaf98fe1c571dfea86d5
Andrey-Grishkov <andreygorm@gmail.com>
2022-04-25 23:30:59 +0300

    feat: make overlay close popup

--- scripts/index.js
+++ scripts/index.js
@@ -95,3 +95,13 @@ profileEditBtn.addEventListener('click', () => {
   userAboutInput.value = profileUserAbout.textContent;
   openPopup(popupProfileEdit);
 });
+
+function overLayClose(event, popupType) {
+  if (event.target === event.currentTarget) {
+    popupType.classList.remove("popup_is-opened");
+  }
+}
+
+popupProfileEdit.addEventListener('click', () => overLayClose(event, popupProfileEdit));
+popupСardAdd.addEventListener('click', () => overLayClose(event, popupСardAdd));
+imagePopup.addEventListener('click', () => overLayClose(event, imagePopup));
