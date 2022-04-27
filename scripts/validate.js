dfe4f97d76be8079d523cd2464da3d1263001214
Andrey-Grishkov <andreygorm@gmail.com>
2022-04-27 20:11:06 +0300

    feat: make validation for 2 popup

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,38 +1,46 @@
 enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__user-input',
-    submitButtonSelector: '.popup__button-submit',
-    inactiveButtonClass: 'popup__button-submit_type_disabled',
     inputErrorClass: 'popup__user-input_type_error',
-    errorClass: 'popup__error_visible'
+    errorClass: 'popup__error_visible',
+    submitButtonSelector: '.popup__button-submit',
+    inactiveButtonClass: 'popup__button-submit_type_disabled'
 });
 
-function enableValidation(config) {
-    const forms = document.querySelectorAll(config.formSelector);
-
+function enableValidation({formSelector, ...rest}) {
+    const forms = document.querySelectorAll(formSelector);
     Array.from(forms).forEach((form) => {
-        
-        form.addEventListener('input', (event) => handleFormInput(event, form, config));
+        form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+        setEventListeners(form, rest);
     });
 }
 
-function handleFormInput(event, form, config) {
+function setEventListeners(form, {inputSelector, ...rest}) {
+    const inputs = form.querySelectorAll(inputSelector);
+    Array.from(inputs).forEach((input) => {
+        input.addEventListener('input', (event) => handleFormInput(event, form, rest));
+        });
+}
+
+function handleFormInput(event, form, {inputErrorClass, errorClass,...rest}) {
     const input = event.target;
     const errorNode = document.querySelector(`#${input.id}-error`);
     if (input.validity.valid) {
         errorNode.textContent="";
-        input.classList.remove(config.inputErrorClass);
-        toggleButton(form, config);
+        input.classList.remove(inputErrorClass);
+        toggleButton(form, rest);
     } else {
         errorNode.textContent=input.validationMessage;
-        errorNode.classList.add(config.errorClass);
-        input.classList.add(config.inputErrorClass);
-        toggleButton(form, config);
+        errorNode.classList.add(errorClass);
+        input.classList.add(inputErrorClass);
+        toggleButton(form, rest);
     }
 }
 
-function toggleButton(form, config) {
-    const buttonSubmit = document.querySelector(config.submitButtonSelector);
+function toggleButton(form, {submitButtonSelector, inactiveButtonClass}) {
+    const buttonSubmit = form.querySelector(submitButtonSelector);
     buttonSubmit.disabled = !form.checkValidity();
-    buttonSubmit.classList.toggle(config.inactiveButtonClass, !form.checkValidity());
+    buttonSubmit.classList.toggle(inactiveButtonClass, !form.checkValidity());
 }
