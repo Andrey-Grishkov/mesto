65c8f46865e24bf78487620da16e2ef3d301764f
Andrey-Grishkov <andreygorm@gmail.com>
2022-06-08 19:55:11 +0300

    refactoring: initial refactoring after configurate webpack

---
+++ src/scripts/Card.js
@@ -0,0 +1,63 @@
+import { openPopup, imagePopup, imageBigSize, imageBigSizeTitle} from '../index.js';
+
+export class Card {
+  _selector;
+  _name;
+  _link;
+  _element;
+  _like;
+
+  constructor(data, selector) {
+    this._selector = selector;
+    this._name = data.name;
+    this._link = data.link;
+  }
+
+  _getItem() {
+    const cardItem = document
+    .querySelector(this._selector)
+    .content
+    .querySelector('.card')
+    .cloneNode(true);
+    this._element = cardItem;
+  }
+
+  generateCard() {
+    this._getItem();
+    this._setEventListeners();
+    this._imageCard = this._element.querySelector('.card__image');
+    this._element.querySelector('.card__title').textContent = this._name;
+    this._imageCard.src = this._link;
+    this._imageCard.alt = this._name;
+    this._like = this._element.querySelector('.card__like');
+    return this._element;
+  }
+
+  _deleteCardHandler() {
+    this._element.remove();
+    this._element = null;
+  }
+
+  _likeBtnHandler() {
+    this._like.classList.toggle("card__like_active")
+  }
+
+  _openImagePopup() {
+    imageBigSize.src = this._link;
+    imageBigSize.alt = this._name;
+    imageBigSizeTitle.textContent = this._name;
+    openPopup(imagePopup);
+  };
+
+  _setEventListeners() {
+    this._element.querySelector('.card__delete').addEventListener('click', () =>{
+      this._deleteCardHandler();
+    });
+    this._element.querySelector('.card__like').addEventListener('click', () =>{
+      this._likeBtnHandler();
+    });
+    this._element.querySelector('.card__image').addEventListener('click', () =>{
+      this._openImagePopup();
+    });
+  }
+}
---
+++ src/scripts/FormValidator.js
@@ -0,0 +1,71 @@
+import { config } from '../index.js';
+
+export class FormValidator {
+  _inputSelector;
+  _inputErrorClass;
+  _errorClass;
+  _buttonSubmit;
+  _inactiveButtonClass;
+  _formElement;
+  _inputError;
+  _formInputs;
+
+  constructor(config, formElement) {
+    this._inputSelector=config.inputSelector;
+    this._inputErrorClass=config.inputErrorClass;
+    this._errorClass=config.errorClass;
+    this._buttonSubmitSelector=config.submitButtonSelector;
+    this._inactiveButtonClass=config.inactiveButtonClass;
+    this._formElement = formElement;
+    this._inputError = Array.from(this._formElement.querySelectorAll('popup__error'));
+    this._formInputs = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    this._buttonSubmit = this._formElement.querySelector(this._buttonSubmitSelector);
+    }
+
+  enableValidation() {
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+    this._toggleActiveBtnSubmit();
+  }
+
+  _setEventListeners() {
+    this._formInputs.forEach((input) => {
+      input.addEventListener('input', (event) => this._handleFormInput(input));
+    });
+  }
+
+  _handleFormInput(input) {
+    const errorNode = document.querySelector(`#${input.id}-error`);
+    if (input.validity.valid) {
+      errorNode.textContent="";
+      input.classList.remove(this._inputErrorClass);
+      this._toggleActiveBtnSubmit();
+    } else {
+      errorNode.textContent=input.validationMessage;
+      errorNode.classList.add(this._errorClass);
+      input.classList.add(this._inputErrorClass);
+      this._toggleActiveBtnSubmit();
+    }
+  }
+
+  deleteErrorAndInputs() {
+    this._formInputs.forEach((input) => {
+      const errorNode = document.querySelector(`#${input.id}-error`);
+      errorNode.textContent = '';
+      input.classList.remove(this._inputErrorClass);
+      input.value="";
+    });
+  }
+
+  submitButtonDisabled() {
+    this._buttonSubmit.disabled = !this._formElement.checkValidity();
+    this._buttonSubmit.classList.add(this._inactiveButtonClass);
+  }
+
+  _toggleActiveBtnSubmit() {
+    this._buttonSubmit.disabled = !this._formElement.checkValidity();
+    this._buttonSubmit.classList.toggle(this._inactiveButtonClass, !this._formElement.checkValidity());
+  }
+}
